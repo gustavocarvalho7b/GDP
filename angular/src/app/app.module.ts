f820d4b90f60f33eb88f1f38cac8892e3df34148
Gustavo Carvalho <gustavocarvalho7b@gmail.com>
2025-06-29 15:29:46 -0300

    Front home completo

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeBarComponent } from './home-bar/home-bar.component';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { CardPublicidadeComponent } from './card-publicidade/card-publicidade.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeBarComponent, CardPublicidadeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    DropdownModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ angular/src/app/card-publicidade/card-publicidade.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-publicidade',
+  templateUrl: './card-publicidade.component.html',
+  styleUrl: './card-publicidade.component.scss'
+})
+export class CardPublicidadeComponent {
+
+}
